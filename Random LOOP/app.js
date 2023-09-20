a6fa5f1786bd38ec7afbdb448b479fbe6b5f326a
Jaykm00 <36683739+theblackpanther00@users.noreply.github.com>
2023-09-20 17:45:50 -0400

    Add files via upload

---
+++ Random LOOP/app.js
@@ -0,0 +1,10 @@
+const exploreBtn = document.getElementById('explore');
+const blogPost = document.getElementById('blog-post')
+const loopBox = document.getElementById('loopbox');
+var container = document.querySelector(".container");
+const gridContainer = document.getElementById('grid-container')
+
+
+
+
+
